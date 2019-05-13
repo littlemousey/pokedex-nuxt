86e40d7be57b0c8aa39653f8e3653ebefc12f997
Ans de Nijs <ansdenijs@gmail.com>
2019-05-13 14:58:45 +0200

    Fix error vnode children parent

--- plugins/cookieStorage.js
+++ plugins/cookieStorage.js
@@ -4,17 +4,15 @@ import * as Cookies from 'js-cookie'
 export default ({ store }) => {
   createPersistedState({
     storage: {
-      getItem: key => Cookies.get(key),
+      getItem: key => {
+        Cookies.getJSON(key)
+      },
       setItem: (key, value) => {
         Cookies.set(key, value, { expires: 1, secure: false })
       },
       removeItem: key => Cookies.remove(key)
     },
     key: 'pokedex-nuxt',
-    paths: [
-      'user.loggedIn',
-      'pokemonList.stateFavoritePokemonList',
-      'favoritePokemon.stateFavoritePokemonList'
-    ]
+    paths: ['user', 'favoritePokemon']
   })(store)
 }
