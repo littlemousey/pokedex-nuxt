c37ffaab607e98623d13e4b6d7aa4080fea9fe55
Ans de Nijs <ansdenijs@gmail.com>
2019-05-27 13:24:03 +0200

    Summaryfavorites component converted to typescript

--- components/SummaryFavorites.vue
+++ components/SummaryFavorites.vue
@@ -34,61 +34,54 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'SummaryFavorites',
-  props: {
-    statePokemonDataList: {
-      type: Array,
-      required: true
-    },
-    stateFavoritePokemonList: {
-      type: Array,
-      required: true
-    }
-  },
-  data: function() {
-    return {
-      maximumList: 10
-    }
-  },
-  computed: {
-    favoriteListLength() {
-      return this.stateFavoritePokemonList.length
-    },
-    morePokemonCanBeAdded() {
-      return (
-        this.favoriteListLength > 1 &&
-        this.favoriteListLength < this.maximumList
-      )
-    }
-  },
-  methods: {
-    startInterval() {
-      const self = this
-      const intervalID = window.setInterval(
-        pickRandomPokemonOrClearInterval,
-        500
-      )
-      function pickRandomPokemonOrClearInterval() {
-        if (self.favoriteListLength < self.maximumList) {
-          self.pickRandomPokemon()
-        } else {
-          clearInterval(intervalID)
-        }
-      }
-    },
-    pickRandomPokemon() {
-      const list = this.statePokemonDataList.filter(function(pokemon) {
-        return !this.stateFavoritePokemonList.includes(pokemon.name)
-      }, this)
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import Pokemon from '~/types/Pokemon.type'
 
-      const number = Math.floor(Math.random() * Math.floor(list.length))
-      this.$emit('addFavoritePokemon', list[number].name)
-    },
-    emptyFavoritePokemonList() {
-      this.$emit('eraseFavoritePokemonList')
+@Component({
+  name: 'SummaryFavorites'
+})
+export default class SummaryFavorites extends Vue {
+  @Prop({ required: true })
+  statePokemonDataList!: Array<Pokemon>
+
+  @Prop({ required: true })
+  stateFavoritePokemonList!: Array<string>
+
+  maximumList: number = 10
+
+  get favoriteListLength(): number {
+    return this.stateFavoritePokemonList.length
+  }
+
+  get morePokemonCanBeAdded(): boolean {
+    return (
+      this.favoriteListLength > 1 && this.favoriteListLength < this.maximumList
+    )
+  }
+  startInterval() {
+    const self = this
+    const intervalID = window.setInterval(pickRandomPokemonOrClearInterval, 500)
+    function pickRandomPokemonOrClearInterval() {
+      if (self.favoriteListLength < self.maximumList) {
+        self.pickRandomPokemon()
+      } else {
+        clearInterval(intervalID)
+      }
     }
+  }
+
+  pickRandomPokemon() {
+    const list = this.statePokemonDataList.filter(pokemon => {
+      return !this.stateFavoritePokemonList.includes(pokemon.name)
+    })
+
+    const number = Math.floor(Math.random() * Math.floor(list.length))
+    this.$emit('addFavoritePokemon', list[number].name)
+  }
+
+  emptyFavoritePokemonList() {
+    this.$emit('eraseFavoritePokemonList')
   }
 }
 </script>
